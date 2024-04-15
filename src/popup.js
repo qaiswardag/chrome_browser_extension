4427dd2fc2cba3e29934be0bacfddb0e42b0bd1f
qaiswardag <qais.wardag@outlook.com>
2024-04-15 13:14:46 +0200

    The capability to save bookmarks

--- src/popup.js
+++ src/popup.js
@@ -1,1 +1,12 @@
-console.log(`Script for popup.js`);
+// adding a new bookmark row to the popup
+const addNewBookmark = () => {};
+
+const viewBookmarks = () => {};
+
+const onPlay = e => {};
+
+const onDelete = e => {};
+
+const setBookmarkAttributes =  () => {};
+
+document.addEventListener("DOMContentLoaded", () => {});
